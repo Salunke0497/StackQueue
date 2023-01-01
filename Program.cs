dc6c4f58d7b2ffc269182de041500d65780e40c9
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2023-01-02 01:16:38 +0530

    welcome

--- Program.cs
+++ Program.cs
@@ -11,17 +11,34 @@ namespace stack
         static void Main(string[] args)
         {
             StackMain stack = new StackMain();
-            Console.WriteLine("enter\n1)Add\n");
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+
+            Console.WriteLine("enter\n1)Add\n2)Display\n3)peak\n4)pop");
             int x = Convert.ToInt32(Console.ReadLine());
             switch (x)
             {
                 case 1:
                     stack.Push(70);
                     stack.Push(30);
-                    stack.Push(56);
-                    Console.ReadLine();
+                    stack.Push(56);                    
+                    break;
+
+                case 2:
+                    Console.WriteLine("Values are : ");
+                    stack.Display();
+                    break;
+
+                case 3:
+                    stack.Peek();
+                    break;
+
+                case 4:
+                    stack.Pop();
                     break;
             }
+            Console.ReadLine();
         }
     }
 }
