be224b8aefbb1cfb0491f9160c6ce53848352952
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2023-01-01 22:47:09 +0530

    welcome

---
+++ StackMain.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace stack
+{
+    internal class StackMain
+    {
+        Node headtop;
+        public StackMain()
+        {
+            this.headtop = null;
+        }
+        public void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.headtop == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.headtop;
+            }
+            this.headtop = node;
+            Console.WriteLine("added the stack value " + value);
+        }
+    }
+}
